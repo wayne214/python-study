ea3743ac5671e790acdcc04dbfc50eb8c1cf798c
wayne <jun_wei_like1@163.com>
2019-05-10 17:07:56 +0800

    python-爬虫学习，豆瓣读书Top 250 数据，修改bug

--- src/douban_books.py
+++ src/douban_books.py
@@ -9,21 +9,24 @@ from bs4 import BeautifulSoup
 # data.encoding='utf-8'
 # print(data.text)
 
-url = 'https://book.douban.com/top250'
+# url = 'https://book.douban.com/top250'
 #给定url并用requests.get()方法来获取页面的text，用etree.HTML()
 #来解析下载的页面数据“data”。
-data = requests.get(url).text
-
-s = etree.HTML(data)
-
-books = s.xpath('//*[@id="content"]/div/div[1]/div/table')
+# data = requests.get(url).text
+#
+# s = etree.HTML(data)
+#
+# books = s.xpath('//*[@id="content"]/div/div[1]/div/table')
 # scores = s.xpath('//*[@id="content"]/div/div[1]/div/table/tr/td[2]/div[2]/span[2]/text()')
 
 for i in range(10):
-    url = 'https://book.douban.com/top250?start={}'.format(i * 25)
+    print('--i-',i)
+    url = 'https://book.douban.com/top250?start={0}'.format(i * 25)
+    print('---url--',url)
     # 总共10页，用 i*25 保证已25为单位递增
     data = requests.get(url).text
     s = etree.HTML(data)
+    books = s.xpath('//*[@id="content"]/div/div[1]/div/table')
 
     for div in books:
         name = div.xpath('./tr/td[2]/div[1]/a/@title')[0]
