d797272c0e2df02c7a3d213a43eef34f8c39ea7b
wayne214 <jun_wei_like1@163.com>
2019-06-22 17:18:22 +0800

    update

--- src/hello.py
+++ src/hello.py
@@ -1,3 +1,13 @@
-class Hello(object):
-    def hello(self, name = 'py'):
-        print ('Hello,', name)
+
+
+
+
+attributes = ['name', 'dob', 'gender']
+values = [['jason', '2000-01-01', 'male'],
+['mike', '1999-01-01', 'male'],
+['nancy', '2001-02-01', 'female']
+]
+
+ss = [dict(zip(attributes, value)) for value in values]
+
+print(ss)
