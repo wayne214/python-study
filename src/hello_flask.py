e43bbd1e60c0014be0734a5e85ab9db38fa50a8e
wayne <jun_wei_like1@163.com>
2019-12-12 18:01:30 +0800

    python flask web框架

--- src/hello_flask.py
+++ src/hello_flask.py
@@ -1,10 +1,12 @@
 from flask import Flask
 
-app = Flask(__name__) # 创建一个应用
+app = Flask(__name__)  # 创建一个应用
+
 
 @app.route('/')
 def index():
     return '<h1>Hello World!</h1>'
+
 
 if __name__ == '__main__':
     app.run()
